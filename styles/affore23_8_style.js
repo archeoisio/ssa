556a4e12b245646dc343344aa4436d3e503db0a2
Archeoisio <m.musio@yahoo.it>
2026-05-25 10:51:31 +0200

    Update affore23_8_style.js

--- styles/affore23_8_style.js
+++ styles/affore23_8_style.js
@@ -1,4 +1,4 @@
-var size = 0;
+var size = 0; 
 var placement = 'point';
 
 var style_affore23_8 = function(feature, resolution){
@@ -22,7 +22,7 @@ var style_affore23_8 = function(feature, resolution){
     }
     if (value >= 0.000000 && value <= 42.890000) {
             style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 0.4 + size,
+        image: new ol.style.Circle({radius: 2.0 + size,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(108,199,29,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -30,7 +30,7 @@ var style_affore23_8 = function(feature, resolution){
     })]
                     } else if (value >= 42.890000 && value <= 45.360000) {
             style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 1.3 + size,
+        image: new ol.style.Circle({radius: 6.5 + size,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(108,199,29,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -38,7 +38,7 @@ var style_affore23_8 = function(feature, resolution){
     })]
                     } else if (value >= 45.360000 && value <= 47.700000) {
             style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 2.2 + size,
+        image: new ol.style.Circle({radius: 11.0 + size,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(108,199,29,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -46,7 +46,7 @@ var style_affore23_8 = function(feature, resolution){
     })]
                     } else if (value >= 47.700000 && value <= 49.790000) {
             style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 3.1 + size,
+        image: new ol.style.Circle({radius: 15.5 + size,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(108,199,29,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -54,7 +54,7 @@ var style_affore23_8 = function(feature, resolution){
     })]
                     } else if (value >= 49.790000 && value <= 61.260000) {
             style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 4.0 + size,
+        image: new ol.style.Circle({radius: 20.0 + size,
             displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(108,199,29,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
